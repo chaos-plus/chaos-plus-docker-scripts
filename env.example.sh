c155e573a8973ebe1b6ae4ad4a52b113b833d672
Mr.Fan <gankcode@gmail.com>
2025-10-11 20:00:13 +0800

    Update env.example.sh

--- env.example.sh
+++ env.example.sh
@@ -7,11 +7,11 @@ export DOMAINS=("example.com")
 export DOMAIN="${DOMAINS[0]}"
 export DOMAIN_REGEX=${DOMAIN/./\\.}
 
-export ACME_DNS="dns_namesilo"
-export ACME_DNS_ID_NAME=""
-export ACME_DNS_ID_VALUE=""
-export ACME_DNS_KEY_NAME="Namesilo_Key"
-export ACME_DNS_KEY_VALUE="xxxx"
+# export ACME_DNS="dns_namesilo"
+# export ACME_DNS_ID_NAME=""
+# export ACME_DNS_ID_VALUE=""
+# export ACME_DNS_KEY_NAME="Namesilo_Key"
+# export ACME_DNS_KEY_VALUE="xxxx"
 
 # dns_dp
 # export ACME_DNS=${ACME_DNS:-"dns_dp"}
@@ -21,7 +21,7 @@ export ACME_DNS_KEY_VALUE="xxxx"
 # export ACME_DNS_KEY_VALUE=${ACME_DNS_KEY_VALUE:-"xxxxxxxxx"}
 
 # dns_ali
-# export ACME_DNS=${ACME_DNS:-"dns_dp"}
+# export ACME_DNS=${ACME_DNS:-"dns_ali"}
 # export ACME_DNS_ID_NAME=${ACME_DNS_ID_NAME:-"Ali_Key"}
 # export ACME_DNS_ID_VALUE=${ACME_DNS_ID_VALUE:-"xxxx"}
 # export ACME_DNS_KEY_NAME=${ACME_DNS_KEY_NAME:-"Ali_Secret"}
