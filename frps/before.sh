48bb4014b6fb6b6bd3a896e1ad4498902536a4a8
Mr.Fan <gankcode@gmail.com>
2025-10-13 18:56:19 +0800

    Update before.sh

--- frps/before.sh
+++ frps/before.sh
@@ -9,17 +9,22 @@
 mkdir -p ${DATA}/frps
 cat << EOF > ${DATA}/frps/frps.toml
 bindPort = 7000
-tcpmuxHTTPConnectPort = 8000
-vhostHTTPPort = 8080
+
+# tcpmuxHTTPConnectPort = 8000
+# vhostHTTPPort = 8080
+
 webServer.addr = "0.0.0.0"
 webServer.port = 7500
 webServer.user = "admin"
 webServer.password = "pa44vv0rd"
+
 auth.token = "${HTPASSWORD:-${PASSWORD:-pa44vv0rd}}"
+
 log.level = "debug"
 log.maxDays = 7
 log.disablePrintColor = false
 detailedErrorsToClient = true
 # 用于 HTTP 请求的自定义 404 页面
 # custom404Page = "/path/to/404.html"
+
 EOF
