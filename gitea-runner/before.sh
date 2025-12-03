d35193a6c368834a434243037ec030c99bb335e8
robotism <gankcode@gmail.com>
2025-12-03 22:15:25 +0800

    update

--- gitea-runner/before.sh
+++ gitea-runner/before.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -e
 
 sudo mkdir -p ${DATA}/gitea-runner/
-sudo chmod 777 ${DATA}/gitea-runner
+sudo chmod 777 ${DATA}/gitea-runner/
+
+sudo rm -rf ${DATA}/gitea-runner/config.yaml
+sudo cp config.yaml ${DATA}/gitea-runner/config.yaml
 
 
 if [ -z "${REGISTRATION_TOKEN}" ]; then
