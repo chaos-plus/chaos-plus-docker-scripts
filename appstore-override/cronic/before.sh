44a8b6218c909dc399faed263e8f8f2770e7a37e
robotism <gankcode@gmail.com>
2025-12-07 09:56:58 +0800

    update

--- appstore-override/cronic/before.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash -e
-
-SRC_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-\cp -rf ${SRC_DIR}/crontab ${DATA}/cronic/
--- appstore/cronic/before.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-
-sudo mkdir -p ${DATA}/cronic
-sudo chmod -R 777 ${DATA}/cronic
